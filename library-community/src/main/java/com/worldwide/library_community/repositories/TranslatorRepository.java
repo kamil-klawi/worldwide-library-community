24a6ffe7a50ab365f8a0cc97a718b2b9b88c9214
kamil-klawi <kamil.klawikowski03@gmail.com>
2024-07-22 22:49:55 +0200

    chore: add translator repository

--- library-community/src/main/java/com/worldwide/library_community/repositories/TranslatorRepository.java
+++ library-community/src/main/java/com/worldwide/library_community/repositories/TranslatorRepository.java
@@ -2,8 +2,14 @@ package com.worldwide.library_community.repositories;
 
 import com.worldwide.library_community.domain.entities.Translator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TranslatorRepository extends JpaRepository<Translator, Long> {
+    @Query("SELECT t FROM Translator t WHERE t.firstName = :firstName")
+    List<Translator> findTranslatorsByName(@Param("firstName") String firstName);
 }
