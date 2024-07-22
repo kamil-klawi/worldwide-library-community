e0e38634a6f19d33010951a518a29289194b4af9
kamil-klawi <kamil.klawikowski03@gmail.com>
2024-07-22 17:00:57 +0200

    chore: add author entity and dto

---
+++ library-community/src/main/java/com/worldwide/library_community/domain/entities/Author.java
@@ -0,0 +1,28 @@
+package com.worldwide.library_community.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "_author")
+public class Author {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+    @Column(name = "secondName")
+    private String secondName;
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
+}
