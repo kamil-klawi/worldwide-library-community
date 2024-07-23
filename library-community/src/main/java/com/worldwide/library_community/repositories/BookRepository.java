82dbc654f0714654e57547e75b96edacb6b0bf97
kamil-klawi <kamil.klawikowski03@gmail.com>
2024-07-23 19:54:21 +0200

    chore: add book repository

--- library-community/src/main/java/com/worldwide/library_community/repositories/BookRepository.java
+++ library-community/src/main/java/com/worldwide/library_community/repositories/BookRepository.java
@@ -2,8 +2,14 @@ package com.worldwide.library_community.repositories;
 
 import com.worldwide.library_community.domain.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query("SELECT b FROM Book b WHERE b.title = :title")
+    Optional<Book> findBookByTitle(@Param("title") String title);
 }
