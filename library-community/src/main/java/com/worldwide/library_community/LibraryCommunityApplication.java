9d6de785951eb779fb6091a06338fb3265a7e071
kamil-klawi <kamil.klawikowski03@gmail.com>
2024-07-24 17:31:17 +0200

    chore(authentication): add email configuration

--- library-community/src/main/java/com/worldwide/library_community/LibraryCommunityApplication.java
+++ library-community/src/main/java/com/worldwide/library_community/LibraryCommunityApplication.java
@@ -2,8 +2,10 @@ package com.worldwide.library_community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync(proxyTargetClass = true)
 public class LibraryCommunityApplication {
 
 	public static void main(String[] args) {
--- library-community/src/main/java/com/worldwide/library_community/services/AuthenticationService.java
+++ library-community/src/main/java/com/worldwide/library_community/services/AuthenticationService.java
@@ -3,8 +3,10 @@ package com.worldwide.library_community.services;
 import com.worldwide.library_community.domain.common.AuthenticationRequest;
 import com.worldwide.library_community.domain.common.AuthenticationResponse;
 import com.worldwide.library_community.domain.common.RegisterRequest;
+import jakarta.mail.MessagingException;
 
 public interface AuthenticationService {
-    AuthenticationResponse register(RegisterRequest request);
+    void register(RegisterRequest request) throws MessagingException;
     AuthenticationResponse authenticate(AuthenticationRequest request);
+    void activateAccount(String token) throws MessagingException;
 }
